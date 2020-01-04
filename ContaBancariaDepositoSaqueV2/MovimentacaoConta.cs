0cbc707de078b4f5f38048183430cdf82d474d7b
hitaichou <holyfernandes@yahoo.co.uk>
2020-01-04 10:54:48 -0300

    alterado propertie por auto-propertie. Removido histórico e adicionado comentários.

--- ContaBancariaDepositoSaqueV2/MovimentacaoConta.cs
+++ ContaBancariaDepositoSaqueV2/MovimentacaoConta.cs
@@ -6,20 +6,6 @@ namespace ContaBancariaDepositoSaqueV2
     class MovimentacaoConta
     {
         //ATRIBUTOS
-        /*
-        public int Conta;
-        public string Titular;
-        public double VlDeposito;
-        //public double _vlSaque;
-        public double VlSaldo = 0;
-        */
-        //Trocando variáveis da classe de public para private
-        /*private int _conta;
-        private string _titular;
-        private double _vlDeposito;
-        private double _vlSaque;
-        private double _vlSaldo = 0;
-        */
         //Usando auto-propertie
         public int Conta { get; private set; }
         private string _titular;
@@ -29,18 +15,12 @@ namespace ContaBancariaDepositoSaqueV2
         //CONSTRUTORES
         public MovimentacaoConta(int conta, string titular)
         {
-            //Conta = conta;
-            //Titular = titular;            
-            //_conta = conta;
             Conta = conta;
             _titular = titular;
         }
 
         public MovimentacaoConta(int conta, string titular, double deposito)
         {
-            //Conta = conta;
-            //Titular = titular;
-            //VlDeposito = deposito;
             Conta = conta;
             _titular = titular;
             Deposito = deposito;
@@ -48,8 +28,6 @@ namespace ContaBancariaDepositoSaqueV2
 
         public MovimentacaoConta(double deposito)
         {
-            //VlDeposito = VlDeposito + deposito;
-            //_vlDeposito = _vlDeposito + deposito;
             Deposito = Deposito + deposito;
         }
 
@@ -67,60 +45,7 @@ namespace ContaBancariaDepositoSaqueV2
                     }
                 }
         }
-        /*public string GetTitular()
-        {
-            return _titular;
-        }
-
-        public void SetTitular(string titular)
-        {   //atribui nome se o nome não for nulo e tamanho for maior que 1
-            if(titular != null && titular.Length > 1)
-            {
-                _titular = titular;
-            }            
-        }*/
-
-        /*public int Conta
-        {
-            get { return _conta; }
-            set { _conta = value; }
-        }
-        */
-
-        /*public double Deposito
-        {
-            get { return _vlDeposito; }
-            set { _vlDeposito = value; }
-        }
-        */
-
-        /*public double GetDeposito()
-        {
-            return _vlDeposito;
-        }
-
-        public void SetDeposito(double deposito)
-        {
-            _vlDeposito = deposito;
-        }*/
-
-        /*public double Saque
-        {
-            get { return _vlSaque; }
-            set { _vlSaque = value; }
-        }
-        */
-
-        /*public double GetSaque()
-        {
-            return _vlSaque;
-        }
-
-        public void SetSaque(double saque)
-        {
-            _vlSaque = saque;
-        }*/
-
+        
         //METODOS
         public void AdicionarSaldo (double deposito)
         {
@@ -130,9 +55,6 @@ namespace ContaBancariaDepositoSaqueV2
 
         public void RemoverSaldo (double saque)
         {
-            //VlSaldo = (VlSaldo - saque) - 5.00;
-            //_vlSaldo = (_vlSaldo - saque) - 5.00;
-            //_vlSaldo = (_vlSaldo - _vlSaque) - 5.00;
             _vlSaldo = (_vlSaldo - Saque) - 5.00;
         }
 
--- ContaBancariaDepositoSaqueV2/Program.cs
+++ ContaBancariaDepositoSaqueV2/Program.cs
@@ -7,8 +7,7 @@ namespace ContaBancariaDepositoSaqueV2
     {
         static void Main(string[] args)
         {
-            //double vldeposito = 0;
-
+            
             Console.Write("Entre o número da conta: ");            
             int conta = int.Parse(Console.ReadLine());            
             Console.Write("Entre o titular da conta: ");
@@ -21,67 +20,53 @@ namespace ContaBancariaDepositoSaqueV2
 
             if (flagDep == "s")
             {
-                Console.Write("Entre o valor do depósito inicial: ");                                
-                //vldeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                //mc.SetDeposito(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
-                //mc.AdicionarSaldo(mc.GetDeposito());
+                Console.Write("Entre o valor do depósito inicial: ");                                                
                 mc.Deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
                 mc.AdicionarSaldo(mc.Deposito);
                 Console.WriteLine();
 
+                //Mostra resultado e pula uma linha
                 Console.WriteLine("Dados da conta:");
                 Console.WriteLine(mc);
 
                 Console.WriteLine();
             }
-            //MovimentacaoConta mc = new MovimentacaoConta(conta, titular, vldeposito);
-
-            //mc.AdicionarSaldo(vldeposito);
-            //mc.AdicionarSaldo(mc.GetDeposito());
-            //Console.WriteLine();
-
-            //Console.WriteLine("Dados da conta:");
-            //Console.WriteLine(mc);
-
-            //Console.WriteLine();
+            //Mostra resultado e pula uma linha
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(mc);
+            Console.WriteLine();
 
             Console.Write("Deseja alterar o nome (s/n)? ");
             string flagNome = Console.ReadLine();
 
             if (flagNome == "s")
             {
-                Console.Write("Entre o novo nome: ");
-                //mc.SetTitular(Console.ReadLine());
-                //Console.WriteLine(mc.GetTitular());
+                Console.Write("Entre o novo nome: ");                
                 mc.Titular = Console.ReadLine();
                 Console.WriteLine(mc.Titular);
             }
             Console.WriteLine();
 
             Console.Write("Entre um valor para depósito: ");
-            //vldeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            //mc.SetDeposito(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
             mc.Deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            //mc.AdicionarSaldo(vldeposito);
-            //mc.AdicionarSaldo(mc.GetDeposito());
+            //Adiciona saldo a conta
             mc.AdicionarSaldo(mc.Deposito);
             Console.WriteLine();
 
+            //Mostra resultado e pula uma linha
             Console.WriteLine("Dados da conta:");
             Console.WriteLine(mc);
 
             Console.WriteLine();
 
-            Console.Write("Entre um valor para saque: ");
-            //double vlSaque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            //mc.SetSaque(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.Write("Entre um valor para saque: ");            
             mc.Saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            //mc.RemoverSaldo(vlSaque);            
-            //mc.RemoverSaldo(mc.GetSaque());
+            //Faz resgate da conta
             mc.RemoverSaldo(mc.Saque);
 
+            //Mostra resultado e pula uma linha
             Console.WriteLine("Dados da conta:");
             Console.WriteLine(mc);
         }
