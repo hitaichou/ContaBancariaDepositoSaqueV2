58c92382f96ea694f95001221c06833e007efa58
hitaichou <holyfernandes@yahoo.co.uk>
2020-01-03 21:02:14 -0300

    Criação de Classe com variáveis globais e métodos para adição e subtração de saldo.

---
+++ ContaBancariaDepositoSaqueV2/MovimentacaoConta.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContaBancariaDepositoSaqueV2
+{
+    class MovimentacaoConta
+    {
+        public int _conta;
+        public string _titular;
+        public double _vlDeposito;
+        public double _vlSaque;
+        public double _vlSaldo = 0;
+
+        public void AdicionarSaldo (double deposito)
+        {
+            _vlSaldo = _vlSaldo + deposito;
+        }
+
+        public void RemoverSaldo (double saque)
+        {
+            _vlSaldo = (_vlSaldo - saque) - 5.00;
+        }
+
+        //Sobreposição        
+        public override string ToString()
+        {
+            return "Conta: "
+                 + _conta
+                 + ", Titular: "
+                 + _titular
+                 + ", Saldo: "
+                 + _vlSaldo.ToString("F2", CultureInfo.InvariantCulture); //tratamento
+                
+        }
+
+    }
+}
--- ContaBancariaDepositoSaqueV2/Program.cs
+++ ContaBancariaDepositoSaqueV2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace ContaBancariaDepositoSaqueV2
 {
@@ -6,7 +7,49 @@ namespace ContaBancariaDepositoSaqueV2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MovimentacaoConta mc = new MovimentacaoConta();
+
+            Console.Write("Entre o número da conta: ");            
+            mc._conta = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");            
+            mc._titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            string flagDep = Console.ReadLine();
+
+            if (flagDep == "s")
+            {
+                Console.Write("Entre o valor do depósito inicial: ");                
+                mc._vlDeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                mc.AdicionarSaldo(mc._vlDeposito);
+                Console.WriteLine();
+
+                Console.WriteLine("Dados da conta:");
+                Console.WriteLine(mc);
+            }
+            Console.WriteLine();
+
+            Console.Write("Entre um valor para deposósito: ");
+            double vlDeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            mc.AdicionarSaldo(vlDeposito);
+            Console.WriteLine();
+
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(mc);
+
+            Console.WriteLine();
+
+            Console.Write("Entre um valor para saque: ");
+            double vlSaque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            mc.RemoverSaldo(vlSaque);            
+
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(mc);
+
+
+
         }
     }
 }
